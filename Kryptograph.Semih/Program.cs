6a07c6588ba2dbc807a08508162886434841e25c
Semih341 <89550028+Semih341@users.noreply.github.com>
2022-06-02 10:14:27 +0200

    Projektdateien hinzufügen.

---
+++ Kryptograph.Semih/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tabelle_test
+{
+    public struct Tabelle
+    {
+        public string Buchstaben;
+        public double prozent;
+    }
+    public static class Tabel
+    {
+        public static Tabelle[] getTable(string datei)
+        {
+            List<Tabelle> liste = new List<Tabelle>();
+            string[] zeilen = File.ReadAllLines(datei);
+
+            foreach (string zeile in zeilen)
+            {
+                string[] data = zeile.Split(';');
+                Tabelle a;
+                a.Buchstaben = data[0];
+                double.TryParse(data[1], out a.prozent);
+
+                liste.Add(a);
+            }
+            return liste.ToArray();
+        }
+    }
+}
